1a30de3a2f7925f8ca3f917a2d67f6ffd104b1f0
tife502 <125226019+tife502@users.noreply.github.com>
2024-09-25 07:24:22 -0500

    Category3

--- script.js
+++ script.js
@@ -112,9 +112,14 @@ function sendForm() {
         data.forEach((category) => {
           const seccion = `
         <section >
-          <h2>${category.name}</h2>
+          <div>
+            <h2>${category.name}</h2>
+            <button type="button" onclick="editPostCategory(${category.category_id})" class="btn btn-outline-warning">EDITAR</button>
+            <button type="button" onclick="deletePostCategory(${category.category_id})" class="btn btn-outline-danger">ELIMINAR</button>
+          </div>
           <ul style = 
-          "list-style-type: none; 
+          "
+          list-style-type: none; 
           display: flex; flex-wrap: wrap;
           justify-content: space-between; 
           padding: 0px; 
@@ -125,77 +130,113 @@ function sendForm() {
           `
           container.innerHTML += seccion;
         });
-          // addlist();
-          fetch("https://fake-api-vq1l.onrender.com/posts", {
-            headers: {
-              Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjExLCJlbWFpbCI6ImouZHVyYW4yQHV0cC5lZHUuY28iLCJpYXQiOjE3MjY4ODQzMTYsImV4cCI6MTc0NDE2NDMxNn0.4PE0SgarbzuJubXxqsVnP4ECYvyr9r4BMet9SwjsiQo",
-            },
-          })
-            //Pasarlo a json
-            .then((response) => response.json())
-            .then((data) => {
-              console.log(data);
-          
-              data.forEach((product) => {
-                const List = document.getElementById(`Lista-${product.category_id}`);
-                console.log(List);
-                const il = document.createElement("li");
-                const images = JSON.parse(product.images);
-                //const img = document.getElementById("img");
-                //img.src = images[0];
-                const myhtml = `
-                <div  id="${product.id}" class="card" style="width: 15rem;  margin: 15px;" >
-                  <img src="${images[0]} " class="card-img-top" alt = "...">
-                  <div class="card-body">
-                  <h5 class="card-title">${product.title}</h5>
-                  <p class="card-text">${product.description}</p>
-                  <p class="card-text">${product.id}</p>
-                    <button type="button" onclick="editPost(${product.id})" class="btn btn-outline-warning">EDITAR</button>
-                    <button type="button" onclick="deletePost(${product.id})" class="btn btn-outline-danger">ELIMINAR</button>
-                  </div>
-                </div>`;
-                il.innerHTML = myhtml;
-                List.appendChild(il);
-              });
-            });
+          addlist();     
+            
       });
 
 
 
     function addlist(){
-      // fetch("https://fake-api-vq1l.onrender.com/posts", {
-      //   headers: {
-      //     Authorization:
-      //       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjExLCJlbWFpbCI6ImouZHVyYW4yQHV0cC5lZHUuY28iLCJpYXQiOjE3MjY4ODQzMTYsImV4cCI6MTc0NDE2NDMxNn0.4PE0SgarbzuJubXxqsVnP4ECYvyr9r4BMet9SwjsiQo",
-      //   },
-      // })
-      //   //Pasarlo a json
-      //   .then((response) => response.json())
-      //   .then((data) => {
-      //     console.log(data);
+      fetch("https://fake-api-vq1l.onrender.com/posts", {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjExLCJlbWFpbCI6ImouZHVyYW4yQHV0cC5lZHUuY28iLCJpYXQiOjE3MjY4ODQzMTYsImV4cCI6MTc0NDE2NDMxNn0.4PE0SgarbzuJubXxqsVnP4ECYvyr9r4BMet9SwjsiQo",
+        },
+      })
+        //Pasarlo a json
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
       
-      //     data.forEach((product) => {
-      //       const List = document.getElementById(`Lista-${product.category_id}`);
-      //       console.log(List);
-      //       const il = document.createElement("li");
-      //       const images = JSON.parse(product.images);
-      //       //const img = document.getElementById("img");
-      //       //img.src = images[0];
-      //       const myhtml = `
-      //       <div  id="${product.id}" class="card" style="width: 15rem;  margin: 15px;" >
-      //         <img src="${images[0]} " class="card-img-top" alt = "...">
-      //         <div class="card-body">
-      //         <h5 class="card-title">${product.title}</h5>
-      //         <p class="card-text">${product.description}</p>
-      //         <p class="card-text">${product.id}</p>
-      //           <button type="button" onclick="editPost(${product.id})" class="btn btn-outline-warning">EDITAR</button>
-      //           <button type="button" onclick="deletePost(${product.id})" class="btn btn-outline-danger">ELIMINAR</button>
-      //         </div>
-      //       </div>`;
-      //       il.innerHTML = myhtml;
-      //       List.appendChild(il);
-      //     });
-      //   });
+          data.forEach((product) => {
+            const List = document.getElementById(`Lista-${product.category_id}`);
+            const il = document.createElement("li");
+            const images = JSON.parse(product.images);
+            //const img = document.getElementById("img");
+            //img.src = images[0];
+            const myhtml = `
+            <div  id="${product.id}" class="card" style="width: 15rem;  margin: 15px;" >
+              <img src="${images[0]} " class="card-img-top" alt = "...">
+              <div class="card-body">
+              <h5 class="card-title">${product.title}</h5>
+              <p class="card-text">${product.description}</p>
+              <p class="card-text">${product.id}</p>
+                <button type="button" onclick="editPost(${product.id})" class="btn btn-outline-warning">EDITAR</button>
+                <button type="button" onclick="deletePost(${product.id})" class="btn btn-outline-danger">ELIMINAR</button>
+              </div>
+            </div>`;
+            il.innerHTML = myhtml;
+            List.appendChild(il);
+          });
+        });
     }
+
+
+    function sendFormCategory() {
+      const id = document.getElementById("saveButtonCategory").dataset.id || null;
+      const name = document.getElementById("name").value;
+      const description = document.getElementById("descriptioncategory").value;
+      const image = document.getElementById("imanumbergecategory").value;
     
+      const bodyCategory = {
+        name,
+        description,
+        image: [image],
+      };
+    
+      // Si hay un id, se está editando un producto; si no, se está creando uno nuevo
+      const method = id ? "PATCH" : "POST";
+      const url = id
+        ? `https://fake-api-vq1l.onrender.com/category/${id}`
+        : "https://fake-api-vq1l.onrender.com/category";
+    
+
+      console.log(bodyCategory);
+
+      fetch(url, {
+        method,
+        headers: {
+          Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjExLCJlbWFpbCI6ImouZHVyYW4yQHV0cC5lZHUuY28iLCJpYXQiOjE3MjY4ODQzMTYsImV4cCI6MTc0NDE2NDMxNn0.4PE0SgarbzuJubXxqsVnP4ECYvyr9r4BMet9SwjsiQo",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bodyCategory),
+      })
+        .then((response) => response.json())
+        .then((updatedCategroy) => {
+          console.log("Category actualizada/creado:", updatedCategroy);
+          location.reload(); // Recargar la página para ver los cambios
+        });
+      }
+
+      function editPostCategory(id) {
+        fetch(`https://fake-api-vq1l.onrender.com/category/${id}`, {
+          headers: {
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjExLCJlbWFpbCI6ImouZHVyYW4yQHV0cC5lZHUuY28iLCJpYXQiOjE3MjY4ODQzMTYsImV4cCI6MTc0NDE2NDMxNn0.4PE0SgarbzuJubXxqsVnP4ECYvyr9r4BMet9SwjsiQo",
+          },
+        })
+          .then((response) => response.json())
+          .then((category) => {
+            // Rellenar los campos del formulario con la info del producto
+            document.getElementById("name").value = category.name;
+            document.getElementById("descriptioncategory").value = category.description;
+            document.getElementById("imanumbergecategory").value = JSON.parse(category.image)[0];
+            // Guardar el id del producto que se está editando
+            document.getElementById("saveButtonCategory").dataset.id = id;
+          });
+      }
+
+      function deletePostCategory(id){
+        fetch(`https://fake-api-vq1l.onrender.com/category/${id}`, {
+          method: "DELETE", 
+          headers: {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjExLCJlbWFpbCI6ImouZHVyYW4yQHV0cC5lZHUuY28iLCJpYXQiOjE3MjY4ODQzMTYsImV4cCI6MTc0NDE2NDMxNn0.4PE0SgarbzuJubXxqsVnP4ECYvyr9r4BMet9SwjsiQo",
+          },
+        })
+        .then( res => res.json())
+        .then( res => {
+          console.log(
+            "respuesta de la api", res
+          )
+          location.reload();
+        })
+      }
